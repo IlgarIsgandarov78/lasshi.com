a8be7e05810c6532feaed41d11987e4b2d1030bf
IlgarIsgandarov78 <ilgar.isgandarov@gmail.com>
2026-05-12 20:53:48 +0200

    Update Supabase URL and anon key

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY";
+const SUPABASE_URL = "https://vddomatxmutsdmmlclxj.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkZG9tYXR4bXV0c2RtbWxjbHhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MDM2MzQsImV4cCI6MjA5NDE3OTYzNH0.bEimNOFIaQpy9T57phvo30cS5fXci6nubvf5Q7imzhk";
 
 export const supabase = createClient(
   SUPABASE_URL,
